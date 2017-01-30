92002292588405e7237e09034afa1f2fab7a45d9
Tobias Koppers <tobias.koppers@googlemail.com>
2017-01-30 09:43:43 +0100

    Documentation

--- app/app.js
+++ app/app.js
@@ -2,11 +2,16 @@
 
 import offlineRuntime from "offline-plugin/runtime";
 
+// Router state
 let currentPage;
 let currentAction;
 
+// The application shell
+// Here it's only a color, but it could be your title bar with logo.
 document.body.style.background = "lightblue";
 
+// Bootstraping for Page Shell.
+// "page" is the already available page
 export function bootstrap(page) {
 	currentPage = page;
 	currentAction = currentPage.open()
@@ -16,6 +21,9 @@ export function bootstrap(page) {
 	registerRouter();
 }
 
+// Bootstrapping for App Shell (or hybrid Page Shell page)
+// "pageName" is only the name of the page.
+// This page will be loaded while bootstrapping
 export function bootstrapAsync(pageName) {
 	currentAction = Promise.resolve();
 	openPage({
@@ -26,6 +34,7 @@ export function bootstrapAsync(pageName) {
 	registerRouter();
 }
 
+// Bind router to events (modern browsers only)
 function registerRouter() {
 	window.addEventListener("popstate", event => {
 		openPage(event.state || {
@@ -34,22 +43,29 @@ function registerRouter() {
 	});
 }
 
+// Start loading loading page
 //const loadingPage = import("./loading/page");
 
+// Router logic for loading and opening a page.
 function openPage(state) {
 	const pageName = state.page;
 	currentAction = currentAction
+		// Close the current page
 		.then(() => currentPage && currentPage.close())
+		// Start loading the next page
 		.then(() => import(`./${pageName}/page`))
+		// Display the loading page while loading the next page
 		/*.then(() => loadingPage
 			.then(loading => loading.open(pageName)
 				.then(() => import(`./${pageName}/page`))
 				.then(page => loading.close().then(() => page))
 		))*/
+		// Open the next page
 		.then(newPage => {
 			currentPage = newPage;
 			return currentPage.open();
 		})
+		// Display error page
 		.catch(err => {
 			return import("./error/page")
 				.then(newPage => {
@@ -60,6 +76,8 @@ function openPage(state) {
 	return currentAction;
 }
 
+// Router logic, Called by pages
+// Starts navigating to another page
 export function navigate(pageName) {
 	const state = { page: pageName };
 	window.history.pushState(state, pageName, `${pageName}.html`);
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = ({ appShell } = {}) => ({
 	entry: appShell ? {
+		// App Shell has only a single entry point
+		// this entry point loads pages with import()
 		shell: "./app/shell.js"
 	} : {
+		// Page Shell requires an entry point per page
 		dashboard: "./app/dashboard.js",
 		login: "./app/login.js",
 		admin: "./app/admin.js"
@@ -24,6 +27,9 @@ module.exports = ({ appShell } = {}) => ({
 		]
 	},
 	plugins: [
+		// Generate a HTML page per page
+		// This could be replace by some server logic or SSR
+		// For the Page Shell each HTML page need to reference the correct entry point
 		new HtmlWebpackPlugin({
 			filename: "dashboard.html",
 			chunks: !appShell && ["dashboard"]
@@ -36,6 +42,7 @@ module.exports = ({ appShell } = {}) => ({
 			filename: "admin.html",
 			chunks: !appShell && ["admin"]
 		}),
+		// Offline support
 		new OfflinePlugin()
 	]
 });
